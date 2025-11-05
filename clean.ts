435c5b29dc170e0388f1f92ac002cfc8a459f4b7
--global <svdanti@gmail.com>
2025-11-05 14:25:13 +0100

    clean

--- clean.ts
+++ clean.ts
@@ -9,14 +9,10 @@ interface Database {
 
 async function clearMessages(): Promise<void> {
   try {
-    // Read the current content of the database file
     const fileContent = await fs.readFile(dbPath, 'utf-8');
     const db: Database = JSON.parse(fileContent);
 
-    // Empty the messages array
     db.messages = [];
-
-    // Write the updated content back to the file
     await fs.writeFile(dbPath, JSON.stringify(db, null, 2), 'utf-8');
 
     console.log('Successfully cleared all messages in db.json');
